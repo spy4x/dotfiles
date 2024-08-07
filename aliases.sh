a46a6e5453888a4a31ee84552b5bfcdd6e3dd38f
Anton Shubin <2spy4x@gmail.com>
2024-08-07 11:46:19 +0800

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,14 +1,12 @@
 # misc START
-alias copy="wl-copy <"
+alias copy="xclip -sel clip <"
 alias rs="rsync -avhzru -P"
 alias rsh="rsync -avhzru -P -e ssh"
-alias ws="webstorm"
 alias size="du -hd1 | sort -hr"
 alias la='ls -la'
 alias list="tree -L 1"
 alias tree="tree -L 2"
 alias up="pnpm up -i -L"
-alias open "xdg-open"
 # misc END
 
 # git START
@@ -33,6 +31,12 @@ function dockerStop(){
   docker ps --filter "name=$1" -q | xargs -r docker stop;
   };
 alias ds="dockerStop"
+function dockerKill(){
+  # Kills containers with name that contains argument $1.
+  # If argument $1 is not provided - kills all docker containers
+  docker ps -a --filter "name=$1" -q | xargs -r docker rm -f;
+  };
+alias dk="dockerKill"
 alias dclean="docker system prune -af"
 # docker END
 
@@ -49,10 +53,4 @@ function to_webp() {
 alias webp='to_webp'
 # webp END
 
-# vpn START
 alias myip="curl ifconfig.me"
-alias vpn_homelab_up="sudo wg-quick up ~/vpn/homelab.conf"
-alias vpn_homelab_down="sudo wg-quick down ~/vpn/homelab.conf"
-alias vpn_us_up="sudo wg-quick up ~/vpn/tod-uptime.conf"
-alias vpn_us_down="sudo wg-quick down ~/vpn/tod-uptime.conf"
-# vpn END
